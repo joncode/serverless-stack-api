2e4cf3012fca181f2c310921225beaa3ee03d1a9
Jon Gutwillig <programcodex@gmail.com>
2020-05-14 15:46:56 -0700

    fixing lint error

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,35 +1,35 @@
 import * as debug from "./debug-lib";
 
 export default function handler(lambda) {
-  return function (event, context) {
-    return Promise.resolve()
-      // Start debugger
-      .then(() => debug.init(event, context))
-      // Run the Lambda
-      .then(() => lambda(event, context))
-      // On success
-      .then((responseBody) => [200, responseBody])
-      // On failure
-      .catch((e) => {
-        // Print debug messages
-        debug.flush(e);
+	return function (event, context) {
+		return Promise.resolve()
+			// Start debugger
+			.then(() => debug.init(event, context))
+			// Run the Lambda
+			.then(() => lambda(event, context))
+			// On success
+			.then((responseBody) => [200, responseBody])
+			// On failure
+			.catch((e) => {
+				// Print debug messages
+				debug.flush(e);
 
-        let errorcode = 500;
-        if (e && e.statusCode) {
-	        errorcode = e.statusCode;
-        }
-        return [errorcode, { error: e.message }];
-      })
-      // Return HTTP response
-      .then(([statusCode, body]) => ({
-        statusCode,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
-        body: JSON.stringify(body),
-      }))
-      // Cleanup debugger
-      .finally(debug.end);
-  };
+				let errorcode = 500;
+				if (e && e.statusCode) {
+					errorcode = e.statusCode;
+				}
+				return [errorcode, { error: e.message }];
+			})
+			// Return HTTP response
+			.then(([statusCode, body]) => ({
+				statusCode,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Credentials": true,
+				},
+				body: JSON.stringify(body),
+			}))
+			// Cleanup debugger
+			.finally(debug.end);
+	};
 }
